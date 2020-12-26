543df84902fdc56a6e405725c63b31fb931d07b2
Song Z <goldstar0906@outlook.com>
2020-12-27 04:44:43 +0800

    apply metatags in all front end pages

--- config/app.php
+++ config/app.php
@@ -198,6 +198,7 @@ return [
         // DataTable
         Yajra\DataTables\DataTablesServiceProvider::class,
 
+
     ],
 
     /*
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,27 +3,21 @@
 
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Clean As New® delivers industrial cleaning services both onsite and offsite. We ensure 100% clean. If we can’t clean it, you don’t pay!">
-  <meta name="author" content="Clean As New®">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   @yield('title')
   <link href="{{ asset('assets/css/bootstrap.min-cg.css?v=20201030') }}" rel="stylesheet">
   <link href="{{ asset('assets/css/styles09.css?v=20201201') }}" rel="stylesheet">
   <link href="{{ asset('assets/css/odometer-theme-car.css?v=20201030') }}" rel="stylesheet">
   <link href="https://cleanasnew.com/" rel="canonical">
-  <meta content="https://www.facebook.com/cangc01/" property="fb:profile_id" />
-  <meta content="en_US" property="og:locale" />
-  <meta content=" Clean As New Powered by Tech Sonic® (China)" property="og:site_name" />
-  <meta content="website" property="og:type" />
-  <meta content="https://cleanasnew.com/" property="og:url" />
-  <meta content="Heat Exchanger Cleaning Services -  Clean As New® (China)" property="og:title" />
-  <meta content="Clean As New® delivers industrial cleaning services both onsite and offsite. We ensure 100% clean. If we can’t clean it, you don’t pay!" property="og:description" />
-  <meta content="{{ asset('assets/img/fb-clean-as-new-in-baytown.jpg') }}" property="og:image" />
-  <meta content="1200" property="og:image:width" />
-  <meta content="630" property="og:image:height" />
-  <meta name="twitter:card" content="summary" />
-  <meta property="fb:app_id" content="2544690392489034" />
+  @if(isset($metadata) && $metadata != null)
+  @foreach($metadata as $key => $r)
+    <meta {!! ($r->name) ? 'name="'.$r->name.'"' : '' !!}  {!! 
+      ($r->content) ? 'content="'.$r->content.'"' : '' !!} {!! 
+      ($r->property) ? 'property="'.$r->property.'"' : '' !!} />
+  @endforeach
+  @endif
+
   <!-- <link rel="alternate" href="https://cleanasnew.com/index.html" hreflang="en" />
   <link rel="alternate" href="https://cleanasnew.com/mx/index.html" hreflang="es-mx" />
   <link rel="alternate" href="https://cleanasnew.com/es/index.html" hreflang="es-es" />
@@ -43,8 +37,7 @@
   <link href="{{ asset('assets/img/favicon-32x32.png') }}" rel="icon" sizes="32x32" type="image/png">
   <link href="{{ asset('assets/img/favicon-16x16.png') }}" rel="icon" sizes="16x16" type="image/png">
   <link href="{{ asset('assets/img/manifest.json') }}" rel="manifest">
-  <meta content="#ffffff" name="msapplication-TileColor">
-  <meta content="{{ asset('assets/img/ms-icon-144x144.png') }}" name="msapplication-TileImage">
+
   <link rel="preconnect" href="//code.jquery.com" />
   <link rel="preconnect" href="//maxcdn.bootstrapcdn.com">
   <link rel="preconnect" href="//www.fonts.googleapis.com" />
