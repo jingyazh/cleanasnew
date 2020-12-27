f0fe920ee3e5bccb87d812b531600ac66257cfb9
Song Z <goldstar0906@outlook.com>
2020-12-27 19:51:28 +0800

    add batch sql

---
+++ config/batch.sql
@@ -0,0 +1,15 @@
+-- insert into posts (locale, title, image_landing, embed, created_at, updated_at) select 'es-us', title, image_landing, embed, created_at, updated_at from posts_copy1 where locale='en';
+-- insert into posts (locale, title, image_landing, embed, created_at, updated_at) select 'mx'   , title, image_landing, embed, created_at, updated_at from posts_copy1 where locale='en';
+-- insert into posts (locale, title, image_landing, embed, created_at, updated_at) select 'es'   , title, image_landing, embed, created_at, updated_at from posts_copy1 where locale='en';
+-- insert into posts (locale, title, image_landing, embed, created_at, updated_at) select 'fr'   , title, image_landing, embed, created_at, updated_at from posts_copy1 where locale='en';
+-- insert into posts (locale, title, image_landing, embed, created_at, updated_at) select 'pt-br', title, image_landing, embed, created_at, updated_at from posts_copy1 where locale='en';
+-- insert into posts (locale, title, image_landing, embed, created_at, updated_at) select 'de'   , title, image_landing, embed, created_at, updated_at from posts_copy1 where locale='en';
+-- insert into posts (locale, title, image_landing, embed, created_at, updated_at) select 'ru'   , title, image_landing, embed, created_at, updated_at from posts_copy1 where locale='en';
+-- insert into posts (locale, title, image_landing, embed, created_at, updated_at) select 'hi-in', title, image_landing, embed, created_at, updated_at from posts_copy1 where locale='en';
+-- insert into posts (locale, title, image_landing, embed, created_at, updated_at) select 'pa-pk', title, image_landing, embed, created_at, updated_at from posts_copy1 where locale='en';
+-- insert into posts (locale, title, image_landing, embed, created_at, updated_at) select 'cn'   , title, image_landing, embed, created_at, updated_at from posts_copy1 where locale='en';
+-- insert into posts (locale, title, image_landing, embed, created_at, updated_at) select 'jp'   , title, image_landing, embed, created_at, updated_at from posts_copy1 where locale='en';
+-- insert into posts (locale, title, image_landing, embed, created_at, updated_at) select 'th'   , title, image_landing, embed, created_at, updated_at from posts_copy1 where locale='en';
+-- insert into posts (locale, title, image_landing, embed, created_at, updated_at) select 'ml'   , title, image_landing, embed, created_at, updated_at from posts_copy1 where locale='en';
+
+
